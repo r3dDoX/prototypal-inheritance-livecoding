40d086a2e5d202b1e1e9c6af078a167bf2d2d677
r3dDoX <patrick@walther-online.ch>
2016-11-16 20:27:31 +0100

    replace Object.create with own implementation

--- prototypes.js
+++ prototypes.js
@@ -1,10 +1,17 @@
+const objectCreate = (proto) => {
+  const obj = {};
+  Object.setPrototypeOf(obj, proto);
+  return obj;
+}
+
+
 const IncrementorProto = {
   inc() {
     console.log(++this.index);
   }
 }
 
-const incrementor = Object.create(IncrementorProto);
+const incrementor = objectCreate(IncrementorProto);
 incrementor.index = 0;
 
 incrementor.inc();
