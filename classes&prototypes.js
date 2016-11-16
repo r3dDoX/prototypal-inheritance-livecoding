8cb74605a0d8eaa4ebf493e70ae706e195503e7b
r3dDoX <patrick@walther-online.ch>
2016-11-16 19:03:18 +0100

    initial commit

---
+++ classes&prototypes.js
@@ -0,0 +1,19 @@
+class LiveTest {
+  constructor() {
+    this.index = 0;
+  }
+
+  gen() {
+    console.log(++this.index);
+  }
+}
+
+const a = new LiveTest();
+const b = new LiveTest();
+a.gen();
+
+a.__proto__.gen = () => {
+  console.log('oops');
+}
+a.gen();
+b.gen();
